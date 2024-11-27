33e243e6b854f60c6ad84c6328e34efc7fc062ef
UkralGovno <loxygamestudio1@gmail.com>
2024-11-27 19:56:04 +0500

    123

--- 8 занятие задача 3.js
+++ 8 занятие задача 3.js
@@ -10,18 +10,18 @@ function ClearAll() {
 function AddTextF1() {
     let div1 = document.getElementById('div1');
     let input1 = document.getElementById('input1');
-    div1.innerText = input1.value;
+    div1.innerText = div1.innerText + input1.value;
     input1.value = ''
 }
 function AddTextF2() {
     let div2 = document.getElementById('div2');
     let input2 = document.getElementById('input2');
-    div2.innerText = input2.value;
+    div2.innerText = div1.innerText + input2.value;
     input2.value = ''
 }
 function AddTextF3() {
     let div3 = document.getElementById('div3');
     let input3 = document.getElementById('input3');
-    div3.innerText = input3.value;
+    div3.innerText = div1.innerText + input3.value;
     input3.value = ''
 }
