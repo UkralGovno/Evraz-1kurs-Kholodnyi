efae96a6baa6d1b38885243ae41589265ea422db
UkralGovno <loxygamestudio1@gmail.com>
2024-12-11 19:49:16 +0500

    123

--- 11 занятие.js
+++ 11 занятие.js
@@ -1,8 +1,19 @@
 let button1 = document.getElementById('button1');
 let button2 = document.getElementById('button2');
 let div1 = document.getElementById('div1');
-div1 = 0;
-div1.innerText = div1;
+let step = 0;
+div1.innerText = step;
+let c = '';
+
+let block1 = document.getElementById('block1');
+let block2 = document.getElementById('block2');
+let block3 = document.getElementById('block3');
+
+let upButton = document.querySelector('#up');
+let downButton = document.querySelector('#down');
+let leftButton = document.querySelector('#left');
+let rightButton = document.querySelector('#right');
+
 
 function makeRed(){
     button1.classList.add('red')
@@ -11,6 +22,7 @@ function makeRed(){
     button2.classList.add('red')
     button2.classList.remove('green');
     button2.classList.remove('blue');
+    c = 'red'
 
 }
 
@@ -21,7 +33,7 @@ function makeGreen(){
     button2.classList.add('green')
     button2.classList.remove('red');
     button2.classList.remove('blue');
-
+    c = 'green'
 }
 
 function makeBlue(){
@@ -31,30 +43,110 @@ function makeBlue(){
     button2.classList.add('blue')
     button2.classList.remove('green');
     button2.classList.remove('red');
-
+    c = 'blue'
 }
 
 function Plus1(){
-    div1.innerText = div1.innerText + 1;
+    step = step + 1;
+    div1.innerText = step;
 }
 function Plus5(){
-    div1.innerText = div1.innerText + 5;
+    step = step + 5;
+    div1.innerText = step;
 }
 function Plus10(){
-    div1.innerText = div1.innerText + 10;
+    step = step + 10;
+    div1.innerText = step;
 }
 function Plus50(){
-    div1.innerText = div1.innerText + 50;
+    step = step + 50;
+    div1.innerText = step;
 }
 function Minus1(){
-    div1.innerText = div1.innerText - 1;
+    step = step - 1;
+    div1.innerText = step;
 }
 function Minus5(){
-    div1.innerText = div1.innerText - 5;
+    step = step - 5;
+    div1.innerText = step;
 }
 function Minus10(){
-    div1.innerText = div1.innerText - 10;
+    step = step - 10;
+    div1.innerText = step;
 }
 function Minus50(){
-    div1.innerText = div1.innerText - 50;
+    step = step - 50;
+    div1.innerText = step;
+}
+
+
+
+function BlockLeft(){
+    if (c === '') {
+        alert('Блок не выбран')
+    }
+    else if (c === 'red') {
+        console.log(block1.style.left, block1.offsetLeft, block1.offsetLeft - step + 'px');
+        block1.style.left = block1.offsetLeft - step + 'px';
+    }
+    else if (c === 'green') {
+        console.log(block2.style.left, block2.offsetLeft, block2.offsetLeft - step + 'px');
+        block2.style.left = block2.offsetLeft - step + 'px';
+    }
+    else if (c === 'blue') {
+        console.log(block3.style.left, block3.offsetLeft, block3.offsetLeft - step + 'px');
+        block3.style.left = block3.offsetLeft - step + 'px';
+    }
+}
+function BlockRight(){
+    if (c === '') {
+        alert('Блок не выбран')
+    }
+    else if (c === 'red') {
+        console.log(block1.style.left, block1.offsetLeft, block1.offsetLeft + step + 'px');
+        block1.style.left = block1.offsetLeft + step + 'px';
+    }
+    else if (c === 'green') {
+        console.log(block2.style.left, block2.offsetLeft, block2.offsetLeft + step + 'px');
+        block2.style.left = block2.offsetLeft + step + 'px';
+    }
+    else if (c === 'blue') {
+        console.log(block3.style.left, block3.offsetLeft, block3.offsetLeft + step + 'px');
+        block3.style.left = block3.offsetLeft + step + 'px';
+    }
+}
+function BlockUp(){
+    if (c === '') {
+        alert('Блок не выбран')
+    }
+    else if (c === 'red') {
+        console.log(block1.style.top, block1.offsetTop, block1.offsetTop - step + 'px');
+        block1.style.top = block1.offsetTop - step + 'px';
+    }
+    else if (c === 'green') {
+        console.log(block2.style.top, block2.offsetTop, block2.offsetTop - step + 'px');
+        block2.style.top = block2.offsetTop - step + 'px';
+    }
+    else if (c === 'blue') {
+        console.log(block3.style.top, block3.offsetTop, block3.offsetTop - step + 'px');
+        block3.style.top = block3.offsetTop - step + 'px';
+    }
+}
+function BlockDown(){
+    if (c === '') {
+        alert('Блок не выбран')
+    }
+    else if (c === 'red') {
+        console.log(block1.style.top, block1.offsetTop, block1.offsetTop + step + 'px');
+        block1.style.top = block1.offsetTop + step + 'px';
+    }
+    else if (c === 'green') {
+        console.log(block2.style.top, block2.offsetTop, block2.offsetTop + step + 'px');
+        block2.style.top = block2.offsetTop + step + 'px';
+    }
+    else if (c === 'blue') {
+        console.log(block3.style.top, block3.offsetTop, block3.offsetTop + step + 'px');
+        block3.style.top = block3.offsetTop + step + 'px';
+
+    }
 }
